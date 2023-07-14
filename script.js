b13331a087046673ebf4c1674e8fa59f24fb20ad
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-07-14 16:58:02 -0300

    Add dark/light mode toggle button

---
+++ script.js
@@ -0,0 +1,13 @@
+const htmlElement = document.documentElement;
+const switchButton = document.querySelector('#switch');
+const imgProfile = document.querySelector('#profile img');
+
+switchButton.addEventListener('click', () => {
+  htmlElement.classList.toggle('light');
+
+  if (htmlElement.classList.contains('light')) {
+    imgProfile.setAttribute('src', './assets/avatar-light.png');
+  } else {
+    imgProfile.setAttribute('src', './assets/avatar.png');
+  }
+});

bcd42d8f488e3c438b99c8c943d4824a80f9b2d1
Carlos Filipe <thecarlosfilipe@gmail.com>
2024-05-14 15:35:57 -0300

    💄 Avatar change

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const imgProfile = document.querySelector('#profile img');
 switchButton.addEventListener('click', () => {
   htmlElement.classList.toggle('light');
 
-  if (htmlElement.classList.contains('light')) {
+  /* if (htmlElement.classList.contains('light')) {
     imgProfile.setAttribute('src', './assets/avatar-light.png');
   } else {
     imgProfile.setAttribute('src', './assets/avatar.png');
-  }
+  } */
 });

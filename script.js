88df5ad3709d400c7442bf774c0b42ea7828edd1
Carlos Filipe <thecarlosfilipe@gmail.com>
2025-04-07 23:55:46 -0300

    ✨ Update video IDs in cards and implement modal iframe for video playback

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const closeModal = document.querySelector('.close-modal');
 const cards = document.querySelectorAll('.cards');
+const iframeEl = document.querySelector('.iframe');
+
+
 
 for (let card of cards) {
   card.addEventListener("click", () => {
+    const videoId = card.getAttribute('id');
     modalOverlay.classList.add('active');
+    iframeEl.src = `https://www.youtube.com/embed/${videoId}`;
   })
 }
 
 closeModal.addEventListener('click', () => {
   modalOverlay.classList.remove('active');
 })
+

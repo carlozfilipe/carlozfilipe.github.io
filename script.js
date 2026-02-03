b0715917d9fc4820a38f1d994d5ce30e8271a51d
Carlos Filipe <thecarlosfilipe@gmail.com>
2026-02-03 19:40:23 -0300

    update 🎉

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const closeModal = document.querySelector('.close-modal');
-const cards = document.querySelectorAll('.card');
-const iframeEl = document.querySelector('.iframe');
+const htmlElement = document.documentElement;
+const switchButton = document.querySelector('#switch');
+const imgProfile = document.querySelector('#profile img');
 
+switchButton.addEventListener('click', () => {
+  htmlElement.classList.toggle('light');
 
-for (let card of cards) {
-  card.addEventListener("click", () => {
-    const videoId = card.getAttribute("id");
-    console.log(videoId);
-    modalOverlay.classList.add('active');
-    iframeEl.src = `https://www.youtube.com/embed/${videoId}`;
-
-    console.log(iframeEl.src)
-  })
-}
-
-closeModal.addEventListener('click', () => {
-  modalOverlay.classList.remove('active');
-  iframeEl.src = "";
-})
-
+  /* if (htmlElement.classList.contains('light')) {
+    imgProfile.setAttribute('src', './assets/avatar-light.png');
+  } else {
+    imgProfile.setAttribute('src', './assets/avatar.png');
+  } */
+});

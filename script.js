a16b6768acbf355c052d81626d2650d8ee8cc1b8
Carlos Filipe <thecarlosfilipe@gmail.com>
2026-04-10 17:14:58 -0300

    refactor: remove commented-out code

--- script.js
+++ script.js
@@ -4,10 +4,4 @@ const imgProfile = document.querySelector('#profile img');
 
 switchButton.addEventListener('click', () => {
   htmlElement.classList.toggle('light');
-
-  /* if (htmlElement.classList.contains('light')) {
-    imgProfile.setAttribute('src', './assets/avatar-light.png');
-  } else {
-    imgProfile.setAttribute('src', './assets/avatar.png');
-  } */
 });
